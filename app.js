e654c13d17197de9a06502bd1446305f7304bf16
angiekimm <eungg22@gmail.com>
2022-09-04 05:01:02 +0900

    challenge #5

--- app.js
+++ app.js
@@ -1,48 +1,29 @@
-// html 가져오는 방법
-// const title = document.getElementById("title");
-
-// open up the element
-// console.dir(title);
-// title.innerText = "Got you!";
-
-// querySelector: returns only first element. not array
-// querySelectorAll: returns all elements
-const h1 = document.querySelector(".hello h1");
-// h1.style.color = "red";
-// console.dir(h1);
-
+// const h1 = document.querySelector(".hello h1");
+const body = document.body;
+console.log(body);
 function handleTitleClick() {
-  h1.style.color = "blue";
+  //   const clickedClass = "clicked";
+  //   if (h1.classList.contains(clickedClass)) {
+  //     h1.classList.remove(clickedClass);
+  //   } else {
+  //     h1.classList.add(clickedClass);
+  //   }
+  h1.classList.toggle("clicked");
 }
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
+// h1.addEventListener("click", handleTitleClick);
+function handleResize() {
+  if (window.innerWidth < 600) {
+    body.classList.remove("yellow");
+    body.classList.remove("purple");
+    body.classList.add("blue");
+  } else if (window.innerWidth >= 600 & window.innerWidth < 800) {
+    body.classList.remove("yellow");
+    body.classList.remove("blue");
+    body.classList.add("purple");
+  } else {
+    body.classList.remove("blue");
+    body.classList.remove("purple");
+    body.classList.add("yellow");
+  }
 }
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("copier!");
-}
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-  alert("ALL GOOOOD");
-}
-// event
-// HTML element
-h1.addEventListener("click", handleTitleClick);
-
-h1.addEventListener("mouseenter", handleMouseEnter);
-
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-// window
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+window.addEventListener("resize", handleResize);
